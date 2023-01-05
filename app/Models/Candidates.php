0d6c93f927fd939929b04170ce4c588d6a8f3182
Annisa <annisasweetbanana19@gmail.com>
2023-01-05 22:33:04 +0700

    4

--- app/Models/Candidates.php
+++ app/Models/Candidates.php
@@ -8,10 +8,22 @@ use Wildside\Userstamps\Userstamps;
 
 class Candidates extends Model
 {
+    use Userstamps;
     use HasFactory;
+    use SoftDeletes;
+
+    public $table = 'project';
+
+    protected $date = [
+      'start_date',
+      'created_at',
+      'updated_at',
+      'deleted_at',   
+    ];
       // field isian
       public $fillable = ['job_id', 'name', 'email', 'phone', 'year'];
       // fitur create, update, delete aktif
+      public $userstamps = true;
       public $timestamps = true;
       public $softDeletes = true;
 }
