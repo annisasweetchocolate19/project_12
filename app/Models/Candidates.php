16db424d5775697e18fe72e0486612d01af22478
Annisa <annisasweetbanana19@gmail.com>
2023-01-05 22:27:16 +0700

    3

--- app/Models/Candidates.php
+++ app/Models/Candidates.php
@@ -8,13 +8,10 @@ use Wildside\Userstamps\Userstamps;
 
 class Candidates extends Model
 {
-    use Userstamps;
+    use HasFactory;
       // field isian
       public $fillable = ['job_id', 'name', 'email', 'phone', 'year'];
       // fitur create, update, delete aktif
-      const CREATED_BY = 'alt_created_by';
-      const UPDATED_BY = 'alt_updated_by';
-      const DELETED_BY = 'alt_deleted_by';
       public $timestamps = true;
       public $softDeletes = true;
 }
--- database/migrations/2023_01_05_122530_create_candidates_table.php
+++ database/migrations/2023_01_05_122530_create_candidates_table.php
@@ -21,9 +21,12 @@ return new class extends Migration
             $table->string('phone')->unique();
             $table->integer('year');
             $table->userstamps();
-            $table->unsignedBigInteger('created_by')->nullable();
-            $table->unsignedBigInteger('updated_by')->nullable();
-            $table->timestamps();
+            $table->unsignedBigInteger( column: 'created_by')->nullable();
+            $table->foreign( columns: 'created_by')->references( columns: 'id')->on( table: 'candidates');            $table->timestamps();
+            $table->unsignedBigInteger( column: 'updated_by')->nullable();
+            $table->foreign( columns: 'updated_by')->references( columns: 'id')->on( table: 'candidates');
+            $table->unsignedBigInteger( column: 'deleted_by')->nullable();
+            $table->foreign( columns: 'deleted_by')->references( columns: 'id')->on( table: 'candidates');
             $table->softDeletes();
             $table->foreignId('job_id')->constrained('jobs')->onDelete('cascade')->onUpdate('cascade');            
         });
